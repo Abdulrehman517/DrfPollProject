c089a318aaf045a55ae1054db4161766f5ea94d8
unknown <abdulrehman517uetian@gmail.com>
2022-01-12 15:16:51 +0500

    test using APIClient

--- polls/tests.py
+++ polls/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from rest_framework.test import APITestCase, APIRequestFactory
+from rest_framework.test import APITestCase, APIRequestFactory, APIClient
 from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
 
@@ -8,6 +8,7 @@ from polls import apiviews
 
 class TestPoll(APITestCase):
     def setUp(self):
+        self.client = APIClient()
         self.factory = APIRequestFactory()
         self.view = apiviews.PollViewSet.as_view({'get': 'list'})
         self.uri = '/polls/'
@@ -29,4 +30,22 @@ class TestPoll(APITestCase):
         response = self.view(request)
         self.assertEqual(response.status_code, 200,
                          'Expected Response Code 200, received {0} instead.'
+                         .format(response.status_code))
+
+    def test_list2(self):
+        self.client.login(username="test", password="test")
+        response = self.client.get(self.uri)
+        self.assertEqual(response.status_code, 200,
+                         'Expected Response Code 200, received {0} instead.'
+                         .format(response.status_code))
+
+    def test_create(self):
+        self.client.login(username="test", password="test")
+        params = {
+            "question": "How are you?",
+            "created_by": 1
+        }
+        response = self.client.post(self.uri, params)
+        self.assertEqual(response.status_code, 201,
+                         'Expected Response Code 201, received {0} instead.'
                          .format(response.status_code))
